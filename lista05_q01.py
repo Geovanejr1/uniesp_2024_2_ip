945f0acb6346ab63935bc1ff1469bd2094020a42
Geovane jr <Geovanejr244@hotmail.com>
2024-09-11 21:45:10 -0300

    lista05_q01.py

---
+++ lista05_q01.py
@@ -0,0 +1,23 @@
+def funcao_processamento():
+# valores aleatorios  
+    x = 10
+    y = 15
+# soma da quantidade passos
+    qtd_passos = x + y 
+# apresentação do resultado
+    print(f'a quantidade de passos para o pirata será de {qtd_passos}')
+
+
+
+if __name__ == '__main__': I
+
+# mensagem de inicio do programa
+
+print("--- --- iniciando programa --- ---")
+
+# processamento realizado pelo programa
+funcao_processamento()
+
+
+# mensagem de fim do programa
+print("--- --- fim do programa --- ---")
